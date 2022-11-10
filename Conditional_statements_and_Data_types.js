0dfe6931d6c36ace21f58db6383dc8532b69482f
Valiantsin Lutchanka <lutchenkovalentin@gmail.com>
2022-11-10 22:22:53 +0100

    add first and second task

---
+++ Conditional_statements_and_Data_types.js
@@ -0,0 +1,22 @@
+    // Perform addition of various types (string + boolean, string + number, number + boolean)
+let str = 'hello'
+let bool = true
+let num = 5
+console.log(str + bool)
+console.log(str + num)
+console.log(num + bool)
+
+    // Perform multiplication of various types (string * boolean, string * number, number * boolean)
+console.log(str * bool)
+console.log(str * num)
+console.log(num * bool)
+
+    // Divide different types (string / boolean, string / number, number / Boolean)
+console.log(str / bool)
+console.log(str / num)
+console.log(num / bool)
+
+    // Perform explicit conversion (number, string, boolean)
+console.log(Number(str))
+console.log(String(bool))
+console.log(Boolean(num))
